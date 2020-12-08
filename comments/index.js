1dbe9d3382c281ccb0fe43fd8fc6df7d36453317
ahmed <you@example.com>
2020-12-08 23:17:00 +0200

    done backend end points

---
+++ comments/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+
+const bodyParser =require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+const postComments= {};
+app.get('/posts/:id/comments',(req,res)=>{
+    
+    res.status(201).send( postComments[req.params.id] || [] );
+
+});
+app.post('/posts/:id/comments',(req,res)=>{
+    let comments= postComments[req.params.id] || [];
+    
+    let id = randomBytes(4).toString('hex');
+    let {content} = req.body;
+    
+    let comment= {
+        id,
+        content
+    }
+
+    comments.push(comment);
+    postComments[req.params.id]= comments;
+    res.status(201).send(comments);
+
+} );
+app.get('/test',(req,res)=>{
+    res.send('working');
+})
+const port=3001;
+
+app.listen(port, ()=>{
+
+    console.log('listening to port ', port);
+})
---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyPraser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+const app = express();
+
+app.use(bodyPraser.json());
+const posts= {};
+
+app.get('/posts', (req,res)=>{
+    res.send(posts)
+})
+
+app.post('/posts', (req,res)=>{
+    console.log(req.body)
+    let id = randomBytes(4).toString('hex');
+    
+    let {title} = req.body;
+    posts[id]= {
+        id,
+        title
+    };
+
+    res.status(201).send(posts[id]);
+
+})
+const port = 3000;
+app.listen(port,()=>{
+    console.log('listening to port ',port );
+})
